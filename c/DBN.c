2fe7ddcc8d181bee15328e47ee05fc3f61d476d3
Mateo William <albertojohn20041103@gmail.com>
2025-11-18 23:38:43 -0500

    Update DBN.c

--- c/DBN.c
+++ c/DBN.c
@@ -14,20 +14,20 @@ double uniform(double min, double max) {
   return rand() / (RAND_MAX + 1.0) * (max - min) + min;  
 }
 
-int binomial(int n, double p) {
-  if(p < 0 || p > 1) return 0;
+// int binomial(int n, double p) {
+//   if(p < 0 || p > 1) return 0;
 
-  int i;
-  int c = 0;
-  double r;
+//   int i;
+//   int c = 0;
+//   double r;
 
-  for(i=0; i<n; i++) {
-    r = rand() / (RAND_MAX + 1.0);
-    if (r < p) c++;
-  }
+//   for(i=0; i<n; i++) {
+//     r = rand() / (RAND_MAX + 1.0);
+//     if (r < p) c++;
+//   }
 
-  return c;
-}
+//   return c;
+// }
 
 double sigmoid(double x) {
   return 1.0 / (1.0 + exp(-x));
